8b2af5cf369dc39528110e3a222783979d1b7b18
Alexander Schalk <Alexanderlschalk@gmail.com>
2020-10-17 23:19:07 -0500

    updated blackjack

--- js/blackjack.js
+++ js/blackjack.js
@@ -4,7 +4,7 @@ var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 var deckB = new Array() //black deck
 var deckR = new Array() //red deck
 var players = new Array()
-var currentPlayer = 0
+var currPlayer = 0
 
 function createDeck (suits, deck) {
   //deck = new Array() remove this
@@ -47,7 +47,7 @@ function createPlayers (num) {
 }
 
 function createPlayersUI () {
-  //document.getElementById('players').innerHTML = ''
+  document.getElementById('players').innerHTML = ''
   for (var i = 0; i < players.length; i++) {
     var div_player = document.createElement('div')
     var div_playerid = document.createElement('div')
@@ -85,10 +85,17 @@ function startblackjack () {
   if ((document.getElementById('btnStart').value = 'Restart')) {
     deckB = new Array()
     deckR = new Array()
+
+    document.getElementById('btnHitMe').disabled = false
+    document.getElementById('btnStay').disabled = false
   }
-  document.getElementById('status').style.display = 'none'
+  playSound(1)
+
+  document.getElementById(
+    'status'
+  ).innerHTML = `It is now Player ${players[currPlayer].ID}'s turn...`
   // deal 2 cards to every player object
-  currentPlayer = 0
+  currPlayer = 0
   createDeck(suitsB, deckB)
   shuffle(deckB)
   createDeck(suitsR, deckR)
@@ -97,23 +104,18 @@ function startblackjack () {
   createPlayersUI()
   dealHands(deckB, 0)
   dealHands(deckR, 1)
-  document
-    .getElementById('player_' + parseInt(currentPlayer))
-    .classList.add('active')
+  document.getElementById('player_' + currPlayer).classList.add('active')
 }
 
 function dealHands (deck, plyr) {
   // alternate handing cards to each player
   // 2 cards each
   for (var i = 0; i < 2; i++) {
-    //    for (var x = 0; x < players.length; x++) {
     var card = deck.pop()
     players[plyr].Hand.push(card)
     renderCard(card, plyr)
     updatePoints()
-    //    }
   }
-  //updateDeck(deck);
 }
 
 function renderCard (card, player) {
@@ -137,7 +139,12 @@ function getPoints (player) {
   for (var i = 0; i < players[player].Hand.length; i++) {
     points += players[player].Hand[i].Weight
   }
-  if (points > 21) {
+
+  players[player].Points = points
+
+  if (points == 21) {
+    end(1)
+  } else if (points > 21) {
     for (var i = 0; i < players[player].Hand.length; i++) {
       //if player hand contains A and points > 21 then -10 points
       if (players[player].Hand[i].Value.includes('A')) {
@@ -146,67 +153,103 @@ function getPoints (player) {
       }
     }
   }
-  players[player].Points = points
   return points
 }
 
+function checkAces () {}
 function updatePoints () {
   for (var i = 0; i < players.length; i++) {
     getPoints(i)
-    document.getElementById('points_' + i).innerHTML = players[i].Points
+    document.getElementById('points_' + i).innerHTML =
+      'Sum: ' + players[i].Points
   }
 }
 
 function hitMe () {
+  playSound(1)
   // pop a card from the deck to the current player
   // check if current player new points are over 21
-  var card = currentPlayer === 0 ? deckB.pop() : deckR.pop()
-  players[currentPlayer].Hand.push(card)
-  renderCard(card, currentPlayer)
+  var card = currPlayer === 0 ? deckB.pop() : deckR.pop()
+  players[currPlayer].Hand.push(card)
+  renderCard(card, currPlayer)
   updatePoints()
   check()
 }
 
 function stay () {
   // move on to next player, if any
-  if (currentPlayer != players.length - 1) {
-    document
-      .getElementById('player_' + currentPlayer)
-      .classList.remove('active')
-    currentPlayer += 1
-    document.getElementById('player_' + currentPlayer).classList.add('active')
+  if (currPlayer != players.length - 1) {
+    document.getElementById('player_' + currPlayer).classList.remove('active')
+    currPlayer += 1
+    document.getElementById('player_' + currPlayer).classList.add('active')
+    document.getElementById(
+      'status'
+    ).innerHTML = `It is now Player ${players[currPlayer].ID}'s turn`
   } else {
-    end()
+    end(2)
   }
 }
 
-function end () {
-  var winner = -1
-  var score = 0
+//condition 1: at least one of the players got a blackjack
+//condition 2: both players stay with a hand below 21
+//condition 3: the current player's hand exceeds 21
+//condition 4: there is a tie
+function end (condition) {
+  document.getElementById('btnHitMe').disabled = true
+  document.getElementById('btnStay').disabled = true
 
-  for (var i = 0; i < players.length; i++) {
-    if (players[i].Points > score && players[i].Points < 22) {
-      winner = i
+  playSound(2)
+
+  if (condition != 3) {
+    var winner = -1
+    var score = 0
+
+    for (var i = 0; i < players.length; i++) {
+      //to settle ties
+      if (players[0].Points === players[1].Points) {
+        document.getElementById('status').innerHTML =
+          'It is a tie!  Neither player wins.'
+        condition = 4
+        break
+      }
+
+      if (players[i].Points > score && players[i].Points < 22) {
+        winner = i
+      }
+      score = players[i].Points
     }
-    score = players[i].Points
+    if (condition != 4) {
+      document.getElementById('status').innerHTML =
+        `Player ${players[winner].ID}` +
+        (condition == 1 ? ' got a Blackjack! ' : "'s hand is higher.") +
+        `  Player ${players[winner].ID} wins!`
+    }
   }
-
-  document.getElementById('status').innerHTML =
-    'Winner: Player ' + players[winner].ID
 }
 
 function check () {
-  if (players[currentPlayer].Points > 21) {
+  if (players[currPlayer].Points == 21) {
+    end(1)
+  } else if (players[currPlayer].Points > 21) {
     document.getElementById('status').innerHTML =
-      'Player: ' + players[currentPlayer].ID + ' LOST'
-    document.getElementById('status').style.display = 'inline-block'
-    end()
+      `Player ${players[currPlayer].ID}'s hand exceeds 21.  Player ` +
+      (players[currPlayer].ID == 1 ? 2 : 1) +
+      ' wins!'
+    end(3)
   }
 }
 
-// function updateDeck(deck) {
-//     document.getElementById('deckcount').innerHTML = deck.length;
-// }
+// function that plays a sound effect
+function playSound (sound) {
+  switch (sound) {
+    case 1:
+      var audio = new Audio('sounds/flip.wav')
+      break
+    case 2:
+      var audio = new Audio('sounds/dingding.wav')
+  }
+  audio.play()
+}
 
 window.addEventListener('load', function () {
   createDeck(suitsB, deckB)
