c703b873cad3682b35095b7ea59cb0ccaa2a3c57
Alexander Schalk <Alexanderlschalk@gmail.com>
2020-10-20 04:09:30 -0500

    updated almost complete

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,14 +1,29 @@
 var suitsB = ['Clubs', 'Spades']
 var suitsR = ['Hearts', 'Diamonds']
-var values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+var values = [
+  '2',
+  '3',
+  '4,',
+  '5',
+  '6',
+  '7',
+  '8,',
+  '9',
+  '10',
+  'J',
+  'Q',
+  'K',
+  'A'
+]
 var deckB = new Array() //black deck
 var deckR = new Array() //red deck
 var players = new Array()
 var currPlayer = 0
+var wins1 = 0,
+  wins2 = 0
 
 function createDeck (suits, deck) {
-  //deck = new Array() remove this
-  console.log(values.length + ' ' + suits.length)
+  // console.log(values.length + ' values, ' + suits.length + ' suits per deck')
 
   for (var i = 0; i < values.length; i++) {
     for (var x = 0; x < suits.length; x++) {
@@ -27,7 +42,7 @@ function createDeck (suits, deck) {
           '.png'
       }
       deck.push(card)
-      console.log(card)
+      //console.log(card)
     }
   }
 }
@@ -47,8 +62,18 @@ function createPlayers (num) {
 }
 
 function createPlayersUI () {
+  document.getElementById('winsDisplay').innerHTML = ''
+  for (var i = 1; i < 3; i++) {
+    var div_display = document.createElement('div')
+    var div_wins = document.createElement('div')
+    div_wins.id = 'totalWins' + i
+    div_wins.innerHTML = 'Wins: ' + (i == 1 ? wins1 : wins2)
+    div_display.appendChild(div_wins)
+    document.getElementById('winsDisplay').appendChild(div_display)
+  }
+
   document.getElementById('players').innerHTML = ''
-  for (var i = 0; i < players.length; i++) {
+  for (var i = 0; i < 2; i++) {
     var div_player = document.createElement('div')
     var div_playerid = document.createElement('div')
     var div_hand = document.createElement('div')
@@ -91,9 +116,7 @@ function startblackjack () {
   }
   playSound(1)
 
-  document.getElementById(
-    'status'
-  ).innerHTML = `It is now Player ${players[currPlayer].ID}'s turn...`
+  turn(1)
   // deal 2 cards to every player object
   currPlayer = 0
   createDeck(suitsB, deckB)
@@ -105,6 +128,11 @@ function startblackjack () {
   dealHands(deckB, 0)
   dealHands(deckR, 1)
   document.getElementById('player_' + currPlayer).classList.add('active')
+}
+
+function turn (num) {
+  document.getElementById('status').innerHTML =
+    `It is now Player ` + num + `'s turn...`
 }
 
 function dealHands (deck, plyr) {
@@ -139,26 +167,27 @@ function getPoints (player) {
   for (var i = 0; i < players[player].Hand.length; i++) {
     points += players[player].Hand[i].Weight
   }
-
   players[player].Points = points
 
-  if (points == 21) {
+  if (points == 21 && players[player].Hand.length == 2) {
     end(1)
   } else if (points > 21) {
     for (var i = 0; i < players[player].Hand.length; i++) {
       //if player hand contains A and points > 21 then -10 points
       if (players[player].Hand[i].Value.includes('A')) {
-        //console.log('Player ' + (player + 1) + '\'s ace value changed to 1')
+        if (i > 0 && points <= 21) {
+          break
+        }
         points -= 10
+        players[player].Points = points
       }
     }
   }
   return points
 }
 
-function checkAces () {}
 function updatePoints () {
-  for (var i = 0; i < players.length; i++) {
+  for (var i = 0; i < 2; i++) {
     getPoints(i)
     document.getElementById('points_' + i).innerHTML =
       'Sum: ' + players[i].Points
@@ -168,7 +197,7 @@ function updatePoints () {
 function hitMe () {
   playSound(1)
   // pop a card from the deck to the current player
-  // check if current player new points are over 21
+  // check if current player's points exceed 21
   var card = currPlayer === 0 ? deckB.pop() : deckR.pop()
   players[currPlayer].Hand.push(card)
   renderCard(card, currPlayer)
@@ -178,33 +207,36 @@ function hitMe () {
 
 function stay () {
   // move on to next player, if any
-  if (currPlayer != players.length - 1) {
+  if (currPlayer != 1) {
     document.getElementById('player_' + currPlayer).classList.remove('active')
     currPlayer += 1
     document.getElementById('player_' + currPlayer).classList.add('active')
-    document.getElementById(
-      'status'
-    ).innerHTML = `It is now Player ${players[currPlayer].ID}'s turn`
+    turn(2)
   } else {
     end(2)
   }
 }
 
 //condition 1: at least one of the players got a blackjack
-//condition 2: both players stay with a hand below 21
+//condition 2: both players stay with a hand not exceeding 21
 //condition 3: the current player's hand exceeds 21
-//condition 4: there is a tie
+//condition 4: there is a tie, while either of conditions 1 or 2 are true
 function end (condition) {
+  // to avoid bogus calculations
+  if (players[1].Hand.length < 2) {
+    return
+  }
+
   document.getElementById('btnHitMe').disabled = true
   document.getElementById('btnStay').disabled = true
 
-  playSound(2)
+  playSound(2) // ding ding sound
 
   if (condition != 3) {
     var winner = -1
     var score = 0
 
-    for (var i = 0; i < players.length; i++) {
+    for (var i = 0; i < 2; i++) {
       //to settle ties
       if (players[0].Points === players[1].Points) {
         document.getElementById('status').innerHTML =
@@ -218,23 +250,30 @@ function end (condition) {
       }
       score = players[i].Points
     }
+
+    // when there is no tie between the scores
     if (condition != 4) {
       document.getElementById('status').innerHTML =
         `Player ${players[winner].ID}` +
         (condition == 1 ? ' got a Blackjack! ' : "'s hand is higher.") +
         `  Player ${players[winner].ID} wins!`
+      updateWins(players[winner].ID)
     }
   }
 }
 
 function check () {
-  if (players[currPlayer].Points == 21) {
+  if (
+    players[currPlayer].Points == 21 &&
+    players[currPlayer].Hand.length == 2
+  ) {
     end(1)
   } else if (players[currPlayer].Points > 21) {
     document.getElementById('status').innerHTML =
       `Player ${players[currPlayer].ID}'s hand exceeds 21.  Player ` +
       (players[currPlayer].ID == 1 ? 2 : 1) +
       ' wins!'
+    updateWins(players[currPlayer].ID == 1 ? 2 : 1)
     end(3)
   }
 }
@@ -249,6 +288,15 @@ function playSound (sound) {
       var audio = new Audio('sounds/dingding.wav')
   }
   audio.play()
+}
+
+// function that updates # of wins per player
+function updateWins (player) {
+  var winner = document.getElementById('totalWins' + player)
+
+  winner.innerHTML = 'Wins: ' + (player == 1 ? ++wins1 : ++wins2)
+  winner.style.color = 'goldenrod'
+  winner.style.border = 'solid 5px goldenrod'
 }
 
 window.addEventListener('load', function () {
